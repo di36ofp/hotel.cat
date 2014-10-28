784eb6748c838ea94849e8e420fceb483048ac09
Diego <info@diego-suarez.es>
2014-10-28 13:51:38 +0100

    add smooth scroll and nav fixed to top

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,33 @@ $(document).ready(function () {
     $("#calendar").toggleClass('active');
   })
   $( window ).scroll(function() {
-    $( "span" ).css( "display", "inline" ).fadeOut( "slow" );
-    console.log(window.scrollY)
+    if(window.scrollY > 130){
+      $('.nav_container').addClass('nav_fixed');
+      $('.slider').addClass('margined-top');
+    }
+    if(window.scrollY < 130){
+      $('.nav_container').removeClass('nav_fixed');
+      $('.slider').removeClass('margined-top');
+    }
+  });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      console.log(target);
+      if($('[name=' + this.hash.slice(1) +']') == "top"){
+        $('html,body').animate({
+          scrollTop: 0
+        }, 1000);
+        return false;
+      }
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 70
+        }, 1000);
+        return false;
+      }
+    }
   });
 })
