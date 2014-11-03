14c29fcf26042b3496736d8ba29ef1e04abaa6e4
Cecilia <Cecilia@MacBook-Pro-de-Cecilia.local>
2014-11-03 11:22:55 +0100

    description photo

--- js/functions.js
+++ js/functions.js
@@ -1,111 +1,184 @@
 // JavaScript Document
-
 $(document).ready(function () {
-
-  $('#booking,.close,.cancel-booking').on('click', toggleModal);
-  $('#arrive,#departure').on('focus', showDatePicker);
-
-  function toggleModal() {
-    $("body").toggleClass('modal-enabled');
-    $("dialog").toggleClass('active');
-    $("header,main,footer").toggleClass('fade');
-  }
-
-  function showDatePicker() {
-    if ( $('#datepicker').length <= 0 ){
-      var datePicker = document.createElement('div');
-      datePicker.setAttribute('id', "datepicker");
-      var parentDatePicker = document.getElementById('booking-form');
-      parentDatePicker.appendChild(datePicker);
-
-      $('#datepicker').datepicker({
-          dateFormat: 'yy-m-d',
-          inline: true,
-          minDate: 0,
-          onSelect: function(dateText, inst) {
-              var date = $(this).datepicker('getDate'),
-                  day  = date.getDate(),
-                  month = date.getMonth() + 1,
-                  year =  date.getFullYear();
-              alert(day + '-' + month + '-' + year);
-          }
-      });
+  /*
+   * Modal booking
+   */
+  (function () {
+    function toggleModal () {
+      $("body").toggleClass('modal-enabled');
+      $("dialog").toggleClass('active');
+      $("header,main,footer").toggleClass('fade');
     }
-  }
 
-  $( window ).scroll(function() {
-    if(window.scrollY > 130){
-      $('.nav_container').addClass('nav_fixed');
-      $('.slider').addClass('margined-top');
-    }
-    if(window.scrollY < 130){
-      $('.here').removeClass();
-      $('.nav_container').removeClass('nav_fixed');
-      $('.slider').removeClass('margined-top');
-    }
-  });
-  $('a[href=#top]').click(function(e) {
-    e.preventDefault;
-    $('html, body').animate({scrollTop:0}, 'slow');
-    return false;
-  });
-  $('a[href*=#]:not([href=#])').click(function() {
-    $('.here').removeClass();
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 70
-        }, 1000);
-        $(this).toggleClass('here');
-        return false;
+    $('#booking,.close,.cancel-booking').on('click', toggleModal);
+  }());
+  /*
+   * Show Datepicker and save date in input
+   */
+  (function () {
+    function showDatePicker ( booking_action ) {
+      var input = booking_action;
+      var booking_action = 'datepicker_' + booking_action;
+      if ( $('#' + booking_action).length <= 0 ){
+        var datePicker = document.createElement('div');
+        datePicker.setAttribute('id', booking_action);
+        var parentDatePicker = document.getElementById('booking-form');
+        parentDatePicker.appendChild(datePicker);
+        $('#' + booking_action).datepicker({
+            dateFormat: 'yy-m-d',
+            inline: true,
+            minDate: 0,
+            onSelect: function (dateText, inst) {
+                var date = $(this).datepicker('getDate'),
+                    day  = date.getDate(),
+                    month = date.getMonth() + 1,
+                    year =  date.getFullYear();
+                    $( "#" + input).val(day + '-' + month + '-' + year);
+                    $( '#' + booking_action ).remove();
+            }
+        });
       }
     }
-  });
+    document.getElementById('arrive').addEventListener('focus', function () {
+        showDatePicker ('arrive');
+    }, false);
 
-  /////////////////////////////////////////
-  var rooms;
-  var table = document.getElementById('table_rates');
-  var http_request = new XMLHttpRequest();
-  http_request.open("GET", "http://localhost:8888/hotel.cat/tmp/rooms.txt", true);
-  http_request.onreadystatechange = function () {
-    var done = 4, ok = 200;
-    if (http_request.readyState === done && http_request.status === ok) {
-        rooms = JSON.parse(http_request.responseText);
-        initialize();
+    document.getElementById('daparture').addEventListener('focus', function () {
+        showDatePicker ('daparture');
+    }, false);
+  }());
+  /*
+   * Booking
+   */
+  (function () {
+    function doNext () {
+      var max_inputs = $('#booking-form').find(':input').length,
+          counter = 0;
+      $('#booking-form').find(':input').
+                        each(function() {
+                          if ( this.value.length < 0 || this.value == "" ){
+                            var error = true;
+                          }
+                          counter++;
+                          if ( !error  && max_inputs == counter ){
+                            $( '#booking-form' ).submit();
+                          }else{
+                            console.log(error);
+                            return false;
+                          }
+                        });
     }
-  };
-  http_request.send(null);
+    document.getElementById('booking_next').addEventListener('click', function () {
+        doNext ();
+    }, false);
+  }());
+  /*
+   * Retrieve data from URL
+   */
+  (function () {
+    var qs = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=', 2);
+            if (p.length == 1)
+                b[p[0]] = "";
+            else
+                b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'));
 
-  function initialize(){
-    rooms.forEach(function ( index ) {
-      draw(index);
-    })
-  }
-  function draw( obj ) {
-    var tbody = document.createElement('tbody');
-    var tr = document.createElement('tr');
-    var th = document.createElement('th');
-    var thId = document.createElement('th');
-    var tdBaja = document.createElement('td');
-    var tdMedia = document.createElement('td');
-    var tdAlta = document.createElement('td');
-    thId.innerHTML = obj.id;
-    tdBaja.innerHTML = obj.temp_baja;
-    tdMedia.innerHTML = obj.temp_media;
-    tdAlta.innerHTML = obj.temp_alta;
+    for (var prop in qs) {
+      console.log(prop + " " +qs[prop]);
+    }
+  }());
+  /*
+   * Navigation effects
+   */
+  (function () {
+    $( window ).scroll(function() {
+      if(window.scrollY > 130){
+        $('.nav_container').addClass('nav_fixed');
+        $('.slider').addClass('margined-top');
+      }
+      if(window.scrollY < 130){
+        $('.here').removeClass();
+        $('.nav_container').removeClass('nav_fixed');
+        $('.slider').removeClass('margined-top');
+      }
+    });
+    $('a[href=#top]').click(function(e) {
+      e.preventDefault;
+      $('html, body').animate({scrollTop:0}, 'slow');
+      return false;
+    });
+    $('a[href*=#]:not([href=#])').click(function() {
+      $('.here').removeClass();
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 70
+          }, 1000);
+          $(this).toggleClass('here');
+          return false;
+        }
+      }
+    });
+  }());
+  /*
+   * Rooms pricing
+   */
+  (function () {
+    var rooms;
+    var table = document.getElementById('table_rates');
+    var http_request = new XMLHttpRequest();
+    http_request.open("GET", "http://localhost:8888/hotel.cat/tmp/rooms.txt", true);
+    http_request.onreadystatechange = function () {
+      var done = 4, ok = 200;
+      if (http_request.readyState === done && http_request.status === ok) {
+          rooms = JSON.parse(http_request.responseText);
+          initialize();
+      }
+    };
+    http_request.send(null);
 
-    tr.appendChild(thId);
-    tr.appendChild(tdBaja);
-    tr.appendChild(tdMedia);
-    tr.appendChild(tdAlta);
-    tbody.appendChild(tr);
-    table.appendChild(tbody);
-  }
+    function initialize(){
+      rooms.forEach(function ( index ) {
+        draw(index);
+      });
+    }
+    function draw( obj ) {
+      var tbody = document.createElement('tbody'),
+          tr = document.createElement('tr'),
+          th = document.createElement('th'),
+          thId = document.createElement('th'),
+          tdBaja = document.createElement('td'),
+          tdMedia = document.createElement('td'),
+          tdAlta = document.createElement('td');
 
-  /////////////////////////////////////////
-  var init = function() {
+      thId.innerHTML = obj.id;
+      tdBaja.innerHTML = obj.temp_baja;
+      tdMedia.innerHTML = obj.temp_media;
+      tdAlta.innerHTML = obj.temp_alta;
+
+      tr.appendChild(thId);
+      tr.appendChild(tdBaja);
+      tr.appendChild(tdMedia);
+      tr.appendChild(tdAlta);
+      tbody.appendChild(tr);
+      table.appendChild(tbody);
+    }
+  }());
+  /*
+   * Carousel
+   */
+  (function () {
+
+    var init = function() {
     var carousel = document.getElementById('carousel'),
         navButtons = document.querySelectorAll('#navigation img'),
         panelCount = carousel.children.length,
@@ -118,11 +191,12 @@ $(document).ready(function () {
           carousel.style.transform = 'rotateY(' + theta + 'deg)';
         };
 
-    for (var i=0; i < 2; i++) {
-      navButtons[i].addEventListener( 'click', onNavButtonClick, false);
-    }
+      for (var i=0; i < 2; i++) {
+        navButtons[i].addEventListener( 'click', onNavButtonClick, false);
+      }
 
-  };
+    };
 
-  window.addEventListener( 'DOMContentLoaded', init, false);
+    window.addEventListener( 'DOMContentLoaded', init, false);
+  }());
 })
